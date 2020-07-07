0139b71b32ace16dc626fb473d23e783b215295f
Sabari Krishna V K <sabari@Sabaris-MacBook-Pro.local>
2020-07-07 11:48:02 -0400

    First commit

---
+++ product/src/schema/schema.graphql.js
@@ -0,0 +1,35 @@
+const typeDefs = `
+  type inOthers {
+    name: String!
+    quantity: String!
+    uuid: String!
+  }
+  
+  type inStocks {
+    name: String!
+    quantity: String!
+    uuid: String!
+  } 
+  type Product {
+    name: String
+    default_image: String
+    new_product: Boolean
+    brand: String!
+    promos: [String]
+    country: String
+    article: String
+    promotion: Boolean
+    price: String
+    in_stocks: [inStocks]
+    in_others: inOthers
+    in_waiting: inOthers
+    currency_name: String 
+  }
+
+  type Query {
+    products: [Product]
+  }
+`;
+
+module.exports = typeDefs;
+
